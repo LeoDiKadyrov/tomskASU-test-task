1237388204a88ba76b6fea11f86b1cd4bd62c65a
Arystan <din02winchester25@gmail.com>
2021-08-30 16:34:31 -0400

    Init
    Added store and json-server

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function App() {
+  const getJSON = async () => {
+    fetch("http://localhost:3000/persons")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  return (
+    <div onClick={getJSON}>
+      abdrahman
+    </div>
+  );
+}
+
+export default App;
+
+
+/* for post/put requests
+  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+*/
---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import personReducer from '../features/person/personSlice.js'
+
+export default configureStore({
+    reducer: {
+        person: personReducer
+    }
+  })
